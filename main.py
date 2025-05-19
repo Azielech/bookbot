d5fb59672bb0575e59f761b7cd68da96b6876f57
Azielech <stafford.samuel@outlook.com>
2025-05-19 09:11:44 -0400

    Bookbot can now receive input and print the Book

--- main.py
+++ main.py
@@ -1,1 +1,7 @@
-print("greetings boots")
+def get_book_text(file_path):
+    with open(file_path) as f:
+        file_contents = f.read()
+    return file_contents
+
+book_text = get_book_text("books/frankenstein.txt")
+print(book_text)
