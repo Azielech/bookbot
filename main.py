2d463d5bf997bde53c89b2f3ecf3a6855e398dde
Azielech <stafford.samuel@outlook.com>
2025-05-20 11:38:49 -0400

    Attempting to generate a report with main.

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
-def get_book_text(file_path):
+def get_book_text(file_path):   
     with open(file_path) as f:
         file_contents = f.read()
     return file_contents
 
-book_text = get_book_text("books/frankenstein.txt") # This give global access to the Book Text.
-print(book_text)
+file_path = "books/frankenstein.txt" #This gives access to the file path for the report generator.
 
-from stats import get_num_words #This give access to the num_words functon inside of main.
+book_text = get_book_text("books/frankenstein.txt") # This give global access to the Book Text.
+#print(book_text)
+
+from stats import get_num_words #This give access to the num_words functon inside of stats.
 word_count = get_num_words(book_text)
-print(f"{word_count} words found in the document")
+#print(f"{word_count} words found in the document")
+
+from stats import get_num_char #This gives access to the num_char function inside of stats.
+char_count = get_num_char(book_text)
+#print(f"{char_count}")
+
+from stats import get_book_report
+sorted_report = get_book_report(char_count)
+#print(sorted_report)
+
+#Report generation
+print("============ BOOKBOT ============")
+print(f"Analyzing book found at {file_path}...")  
+print("----------- Word Count ----------")
+print(f"Found {word_count} total words")  
+print("--------- Character Count -------")
+for report_data in sorted_report:
+    sorted_char = report_data["char"]
+    sorted_num = report_data["num"]
+    if sorted_char.isalpha():
+      print(f"{sorted_char}: {sorted_num}")
+print("============= END ===============")
--- stats.py
+++ stats.py
@@ -1,3 +1,27 @@
 def get_num_words(text):
     word_list = text.split()
     return len(word_list)
+
+
+def get_num_char(text):
+    word_list = text.lower()
+    counts = {}
+
+    for char in word_list:
+        if char not in counts:
+            counts[char] = 1
+        else:
+            counts[char] += 1
+    return counts
+
+def get_book_report(book_data): 
+    dictionaries = []
+
+    for char, num in book_data.items():
+        new_dict = {"char": char, "num": num}  # Create a new dictionary
+        dictionaries.append(new_dict)  # Append the dictionary to the list
+        dictionaries.sort(reverse=True, key=sort_report)
+    return dictionaries
+
+def sort_report(dict):
+    return dict["num"]
