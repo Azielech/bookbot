79e84c1ca5cfa34b6c3f0976d8fe13bf1123b1af
Azielech <stafford.samuel@outlook.com>
2025-05-20 12:09:28 -0400

    Added the sys module to work with multiple books.

--- main.py
+++ main.py
@@ -1,24 +1,33 @@
+import sys
+
+if len(sys.argv) < 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
 def get_book_text(file_path):   
     with open(file_path) as f:
         file_contents = f.read()
     return file_contents
 
-file_path = "books/frankenstein.txt" #This gives access to the file path for the report generator.
+file_path = sys.argv #This gives access to the file path for the report generator.
 
-book_text = get_book_text("books/frankenstein.txt") # This give global access to the Book Text.
-#print(book_text)
+book_text = get_book_text(sys.argv) # This give global access to the Book Text.
+#print(book_text) 
+#^ --------------Release for debugging
 
 from stats import get_num_words #This give access to the num_words functon inside of stats.
 word_count = get_num_words(book_text)
 #print(f"{word_count} words found in the document")
+#^ --------------Release for debugging
 
 from stats import get_num_char #This gives access to the num_char function inside of stats.
 char_count = get_num_char(book_text)
 #print(f"{char_count}")
+#^ --------------Release for debugging
 
 from stats import get_book_report
 sorted_report = get_book_report(char_count)
 #print(sorted_report)
+#^ --------------Release for debugging
 
 #Report generation
 print("============ BOOKBOT ============")
